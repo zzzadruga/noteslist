f5b7f98fe366439e3a75e0e73b748497bff062a3
zzzadruga <zzzadruga@gmail.com>
2018-02-14 17:23:17 +0300

    Added interface "NoteService"

---
+++ src/main/java/ru/zzzadruga/service/NoteService.java
@@ -0,0 +1,11 @@
+package ru.zzzadruga.service;
+
+import org.springframework.data.domain.Page;
+import ru.zzzadruga.model.Note;
+
+public interface NoteService {
+    void save(Note note);
+    Note getNoteById(long id);
+    void remove(long id);
+    Page<Note> getPage(int pageNumber, String searchString);
+}
